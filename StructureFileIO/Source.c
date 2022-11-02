568f54ac4ff99ff53a7ca9d3f3639094e0aaf089
Hayden Auterhoff <Hauterhoff6824@conestogac.on.ca>
2022-11-02 17:30:46 -0400

    Made StructureFileIO worksheet

---
+++ StructureFileIO/Source.c
@@ -0,0 +1,42 @@
+//Read and Write to a file an array of strcutures (Load and Save)
+
+//Let's build a program that stores information like a database (Array of Structures)
+//In the Data Entry struct, the information: First Name, Last Name, Address, and Age are stored
+//Use another strcut containing the Data Entry struct to generate the array with a boolean status (Filled or not)
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAXLEN 20
+
+//Create two structs, one from creating the structure array and one for holding individual data entries
+
+//Initialize Functions: LoadData, SaveData, PrintArray, EnterDataEntry
+
+int main()
+{
+	//Initialize both structures, one as an array
+
+	//Function calls (Load Data)
+
+	//User input for Data Entry
+
+	//Function call to enter data entry in an unfilled array
+
+	//Function call (Save Datra
+
+	return 0;
+}
+
+
+//LoadData Function
+
+//SaveData Function
+
+//PrintArray Function
+
+//EnterDataEntry
